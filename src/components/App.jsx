178b3d638cd3f20ed8f330e641cbde3402747435
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-05-09 16:52:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { lazy, useEffect } from 'react';
+import { lazyt } from 'react';
 import { Layout } from "./Layout/Layout";
 
 
